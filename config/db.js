2423fce6c9e123cd736904411d980e735ca3b6d8
lukezsmith <lukezachsmith@gmail.com>
2020-05-06 23:49:53 +0100

    config unignored, gulp deployment issue hopefully fixed?

---
+++ config/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// const db = 'dhfuisdhiud';
+
+class Database {
+  // Connects to the MongoDB database
+  async connectDB () {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+
+    return true;
+  } catch (err) {
+    console.error('Unable to connect to database, please try again later.');
+    console.error(err.message);
+    return false;
+  }
+  };
+
+  disconnectDB () {
+    mongoose.connection.close();
+  };
+}
+
+module.exports = Database;
